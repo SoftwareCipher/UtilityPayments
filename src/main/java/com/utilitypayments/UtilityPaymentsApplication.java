1e959f01f5ffcc48d0cdd336e3c53f28e160d46b
observerUA <kirillkuznetsov181@gmail.com>
2024-09-20 23:20:34 +0300

    utility payment

---
+++ src/main/java/com/utilitypayments/UtilityPaymentsApplication.java
@@ -0,0 +1,13 @@
+package com.utilitypayments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtilityPaymentsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UtilityPaymentsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/utilitypayments/UtilityPaymentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utilitypayments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtilityPaymentsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
