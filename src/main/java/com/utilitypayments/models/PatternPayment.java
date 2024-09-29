a1df9dd3413c0a796dd5ba892501235351721acc
observerUA <kirillkuznetsov181@gmail.com>
2024-09-29 22:13:29 +0300

    patternPyment

---
+++ src/main/java/com/utilitypayments/models/PatternPayment.java
@@ -0,0 +1,19 @@
+package com.utilitypayments.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "pattern_payment")
+public class PatternPayment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String namePattern;
+    private String Iban;
+    private String infoAboutPattern;
+    @ManyToOne
+    @JoinColumn(name = "user_entity", referencedColumnName = "id")
+    private UserEntity user;
+}
